39fb79a4a706084014dab6fadc7d8cb621dc43a5
medivh <xu_miao2013@sina.com>
2019-03-08 17:39:34 +0800

    修改一点小瑕疵

--- setup.py
+++ setup.py
@@ -6,23 +6,23 @@ from setuptools import setup, find_packages
 
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
-with open('baseAPI/__init__.py', 'rb') as f:
+with open('base/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
 setup(
     name='AT-M',
     version=version,
-    url='https://github.com/medivhXu/interfaceFramWorl-tester',
+    url='https://github.com/medivhXu/AT-interface',
     license='Apache License 2.0',
-    author='medivhXu',
+    author='Medivh Xu',
     author_email='medivh_xu@outlook.com',
     description='接口自动化框架',
     long_description=__doc__,
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=['requests', 'pymysql', 'pyyaml', 'openpyxl', 'parameterized', 'pyDes', 'ruamel.yaml'],
+    install_requires=['requests', 'pymysql', 'pyyaml', 'parameterized', 'pyDes', 'ruamel.yaml'],
     classifiers=[
         'Intended Audience :: Developers',
         'Operating System :: POSIX :: Linux',
