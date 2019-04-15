2568386dc638b661858014b69ae0db757218727d
medivh <xu_miao2013@sina.com>
2019-04-15 20:55:49 +0800

    优化

--- analysis/send_email.py
+++ analysis/send_email.py
@@ -5,7 +5,7 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.utils import formataddr
-from configElement.yaml_data import ConfYaml
+from configElement.yaml_manager import ConfYaml
 from base.log import logged, LOGGER
 
 
