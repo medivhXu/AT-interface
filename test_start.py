8b2ee76dcb195d4291d98a2281332ea95da53301
medivh <xu_miao2013@sina.com>
2019-04-15 15:42:52 +0800

    修改配置信息及优化样本文件

--- test_start.py
+++ test_start.py
@@ -13,10 +13,27 @@ from analysis.comparison_results import differences
 from module.get_variable import get_var, push_var
 from module.get_msg_from_db import get_msg_from_db
 
+
+"""
+             ┏┓   ┏┓
+            ┏┛┻━━━┛┻┓
+            ┃   ☃   ┃
+            ┃ ┳┛ ┗┳ ┃
+            ┃   ┻   ┃
+            ┗━┓   ┏━┛
+              ┃      ┗━━━┓
+              ┃  神兽保佑 ┣┓
+              ┃　永无BUG！┏┛
+              ┗┓┓┏━┳┓ ┏┛
+               ┃┫┫  ┃┫┫
+               ┗┻┛  ┗┻┛
+"""
+
+
 # debug data
-par = [{'case_no': 1, 'case_name': '短信', 'url': 'https://test-acs.czb365.com/services/v3/begin/sendMsg',
+par = [{'case_no': 1, 'case_name': '短信', 'path': 'https://test-acs.czb365.com/services/v3/begin/sendMsg',
         'method': 'post', 'port': None, 'data': {'phone': '$phone'}, 'checkpoint': {'text': {'code': 200}}},
-       {'case_no': 1, 'case_name': '登录', 'url': 'https://test-acs.czb365.com/services/v3/begin/loginAppV4',
+       {'case_no': 2, 'case_name': '登录', 'path': 'https://test-acs.czb365.com/services/v3/begin/loginAppV4',
         'method': 'post', 'port': None, 'data': {'phone': '%phone', 'code': '&get_msg_from_db(%phone)'},
         'checkpoint': {'text': {'code': 200, 'token': '$&token'}}}
        ]
@@ -25,6 +42,7 @@ par = [{'case_no': 1, 'case_name': '短信', 'url': 'https://test-acs.czb365.com
    $：表示从配置文件中取值
    %：代表从全局变量中取值
    &：表示运行系统方法
+   $&：表示从上一个接口中取数据
 """
 
 TEMPORARY_VARIABLE = {}
@@ -42,12 +60,12 @@ class Test(unittest.TestCase, TestRunner):
         pass
 
     def test_run(self):
-        LOGGER.info('No: {} {}'.format(self.case_no, self.case_name))
+        LOGGER.info('*************** No: {} {}'.format(self.case_no, self.case_name))
         if self.method == 'get':
             # get方法还没调好
             data = CollageStr(self.data).sign_str(self.data['phone'])
 
-            res = requests.get(self.url, data, verify=True)
+            res = requests.get(self.path, data, verify=True)
             LOGGER.info('response: {}'.format(res.text))
             if res.status_code != requests.codes.ok:
                 dict_res = json.dumps(res.text)
@@ -58,9 +76,9 @@ class Test(unittest.TestCase, TestRunner):
                                 differences(self.checkpoint[po][var], dict_res)
                 else:
                     LOGGER.warning("[-]用例: {}, 请求地址: {}, 请求参数: {}, 响应body: {}"
-                                   .format(self.case_no, self.url, res.status_code, res.text))
+                                   .format(self.case_no, self.path, res.status_code, res.text))
             else:
-                LOGGER.info("[+]用例: {}, 请求地址: {}, 参数: {} , 响应: {}".format(self.case_no, self.url, self.data, res.text))
+                LOGGER.info("[+]用例: {}, 请求地址: {}, 参数: {} , 响应: {}".format(self.case_no, self.path, self.data, res.text))
         elif self.method == 'post':
             # 取前置条件所需的变量
             for d in self.data:
@@ -100,7 +118,7 @@ class Test(unittest.TestCase, TestRunner):
 
             LOGGER.info('***** request: {}'.format(self.data))
             start_time = datetime.datetime.now()
-            res = requests.post(self.url, data, verify=True)
+            res = requests.post(self.path, data, verify=True)
             end_time = datetime.datetime.now() - start_time
             LOGGER.info('***** response: {}, 响应时间: {} 秒!'.format(res.text, end_time))
 
@@ -121,9 +139,9 @@ class Test(unittest.TestCase, TestRunner):
 
                 else:
                     LOGGER.warning("[-]用例: {}, 请求地址: {}, 请求参数: {}, 响应body: {}"
-                                   .format(self.case_no, self.url, res.status_code, res.text))
+                                   .format(self.case_no, self.path, res.status_code, res.text))
             else:
-                LOGGER.info("[+]用例: {}, 请求地址: {}, 参数: {} , 响应: {}".format(self.case_no, self.url, self.data, res.text))
+                LOGGER.info("[+]用例: {}, 请求地址: {}, 参数: {} , 响应: {}".format(self.case_no, self.path, self.data, res.text))
         else:
             raise MethodException("方法错误，不支持{}方法!".format(self.method))
 
