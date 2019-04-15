adabb6d6a3faabb2353100a9d17117573249eab0
medivh <xu_miao2013@sina.com>
2019-04-15 15:35:40 +0800

    加注释

--- base/encrypt.py
+++ base/encrypt.py
@@ -20,6 +20,11 @@ class Encryption(object):
     @staticmethod
     @logged
     def md5(string: str) -> str:
+        """
+        md5加密
+        :param string: 字符串
+        :return:
+        """
         md = hashlib.md5()
         md.update(string.encode(encoding='utf-8'))
         _md5_msg = str(md.hexdigest())
@@ -28,6 +33,12 @@ class Encryption(object):
     @staticmethod
     @logged
     def des(string: str, secret_key: str) -> str:
+        """
+        des加密
+        :param string: 字符串
+        :param secret_key: 密钥
+        :return:
+        """
         _key = des(secret_key, padmode=PAD_PKCS5)
         _des_str = base64.b64encode(_key.encrypt(json.dumps(string)))
         return _des_str
