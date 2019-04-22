35ee517aa2fbf45df79467596fa883b0217aa3f9
medivh <xu_miao2013@sina.com>
2019-04-22 19:45:01 +0800

    v1.0.2版本更新

--- base/__init__.py
+++ base/__init__.py
@@ -9,3 +9,15 @@ __version__ = '1.0.0'
 1.0.0 version update:
 * 首次提交 可以做基本的接口测试使用
 """
+
+"""
+1.0.1 version update:
+* 修复requests依赖中,urllib3中的高危漏洞
+"""
+
+"""
+1.0.2 version update:
+* 优化字符串拼接类中，字符串排序方法名称
+* 修复start脚本中的取用户token等参数错误
+* 修复start脚本中请求参数写死时抛异常，缺少必要参数
+"""
